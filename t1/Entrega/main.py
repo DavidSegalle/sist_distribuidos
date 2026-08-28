283e26d141c62f9cc15fefcecd9470843448dc84
Pedro Neves <pneves.ciet@alelo.com.br>
2026-08-28 20:46:59 -0300

    MS Entrega initial setup

---
+++ t1/Entrega/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+class Entrega:
+    def __init__(self):
+        connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+        self.channel = connection.channel()
+
+        self.channel.exchange_declare(exchange='ecommerce', exchange_type='direct')
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.consumer_queue = result.method.queue
+
+        self.channel.queue_bind(exchange='ecommerce', queue=self.consumer_queue,
+                   routing_key="pagamento.aprovado")
+        self.channel.queue_bind(exchange='ecommerce', queue=self.producer_queue,
+                   routing_key="pedido.enviado")
+
+        self.consume()
+
+
+    def callback(self,ch, method, properties, body):
+        print(f" [x] {method.routing_key}:{body}")
+        print(f" Gerando nota...")
+        msg = "Teste"
+        self.publish(msg)
+    
+    def consume(self):
+        self.channel.basic_consume(
+        queue=self.consumer_queue, on_message_callback=self.callback, auto_ack=True)
+        print("Started consuming")
+        self.channel.start_consuming()
+
+    def publish(self,message):
+        self.channel.basic_publish(
+        exchange='direct_logs', routing_key="pedido.enviado", body=message)
