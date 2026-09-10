18756226f3c417648e1f867a810b17361161351c
DavidSegalle <davidsegalle@alunos.utfpr.edu.br>
2026-09-10 14:59:27 -0300

    Basic entrega working and a tester for good measure

---
+++ README.md
@@ -0,0 +1,6 @@
+# Como rodar um microsserviço
+
+```bash
+cd sist_distribuidos/t1
+python3 -m Promocoes.ads
+```
--- t1/Entrega/tester.py
+++ t1/Entrega/tester.py
@@ -1,23 +1,13 @@
-#!/usr/bin/env python
 import pika
 import sys
-import time
 import json
 
 from key_manager.generate_keys import KeyManager
 
-# O Microsserviço Entrega é responsável pelo gerenciamento da emissão de notas e da
-# entrega dos produtos.
-# O serviço deverá consumir o evento pagamento.aprovado.
-# Após receber esse evento, deverá realizar as operações necessárias para emissão da nota
-# e preparação da entrega. Após o processamento, deverá publicar um novo evento
-# utilizando a routing key pedido.enviado, informando que o pedido foi enviado.
-
-
-class Entrega:
+class EntregaTest:
     def __init__(self):
 
-        self.key_manager = KeyManager("entrega")
+        self.key_manager = KeyManager("pagamento")
 
         connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost'))
@@ -29,7 +19,9 @@ class Entrega:
         self.consumer_queue = result.method.queue
 
         self.channel.queue_bind(exchange='ecommerce', queue=self.consumer_queue,
-                   routing_key="pagamento.aprovado")
+                   routing_key="pedido.enviado")
+
+        self.publish("Hello")
 
         self.consume()
 
@@ -37,15 +29,7 @@ class Entrega:
     def callback(self,ch, method, properties, body):
         print(f" [x] {method.routing_key}:{body}")
         print(f" Gerando nota...")
-
-        info = json.loads(body)
-        
-        if self.key_manager.check_signature(info["message"], info["signature"], "pagamento"):
-            print(f" [x] {method.routing_key}:{info["message"]}")
-        else:
-            print("Falsified signature")
         msg = "Teste"
-        time.sleep(2)
         self.publish(msg)
     
     def consume(self):
@@ -57,11 +41,11 @@ class Entrega:
     def publish(self,message):
 
         signature = self.key_manager.sign(message)
-        
+                
         signed_message = {"message": message, "signature": signature}
 
         self.channel.basic_publish(
-        exchange='ecommerce', routing_key="pedido.enviado", body=json.dumps(signed_message))
+        exchange='ecommerce', routing_key="pagamento.aprovado", body=json.dumps(signed_message))
 
 if __name__ == "__main__":
-    e = Entrega()
+    e = EntregaTest()
